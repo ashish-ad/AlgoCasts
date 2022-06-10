b1edd325e0df8cb804fa35ffb7972a978588028f
ashish-ad <73051572+ashish-ad@users.noreply.github.com>
2022-06-10 20:08:27 +0530

    Reverse String and Debugger

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,44 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    debugger;
+    return str.split('').reduce((prev,curr)=> curr + prev
+    , '');
+}    //   Here Reduce method takes two first one is a reducer function which takes three argument in which two are mandatory and other one is optional argument i.e. previousValue, currentValue and array || For more information go to following link **https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce**
+
+reverse('ashish');
 
 module.exports = reverse;
+
+//            1st Solution
+// function reverse(str) {
+//     
+//     return str.split('').reverse().join('');
+// }
+
+//            2nd Solution
+// function reverse(str) {
+//   var result = "";
+//   for (let i = str.length - 1; i >= 0; i--) {
+//     result += str[i];
+//   }
+//   return result;
+// }
+
+//           3rd Solution
+// function reverse(str) {
+//   var result = "";
+//   for (let character of str) {
+//     result = character + result;
+//   }
+//   return result;
+// }
+
+
+//           4th Solution
+// function reverse(str) {
+//     return str.split('').reduce((previousValue,currentValue)=>{
+//         return currentValue + previousValue;
+//     }, '');
+// }    //   Here Reduce method takes two first one is a reducer function which takes three argument in which two are mandatory and other one is optional argument i.e. previousValue, currentValue and array   || For more information go to following link **https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce**
