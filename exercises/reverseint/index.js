034f1154628f5383308c5fd2f2312f4e3beb7a7e
ashish-ad <73051572+ashish-ad@users.noreply.github.com>
2023-08-07 08:07:40 +0530

    Added one more algorithm for reversing intergers

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,19 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  if (n === 0) return 0
+  let reverseNumber
+  if (Math.sign(n)===-1) {
+    reverseNumber = n.toString().split('').reverse()
+    const sign = reverseNumber.pop()
+    reverseNumber.unshift(sign)
+    reverseNumber = reverseNumber.join('')
+  } else {
+    reverseNumber = n.toString().split('').reverse()
+    reverseNumber = reverseNumber.join('')
+  }
+  return Number(reverseNumber)
+}
 
 module.exports = reverseInt;
