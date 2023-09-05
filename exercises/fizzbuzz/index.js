a5d197ec19673386bf2808c842ad9d68f5473da6
ashish-ad <73051572+ashish-ad@users.noreply.github.com>
2023-09-05 13:40:28 +0530

    solved two problems of fizzbuzz and maxchars

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,21 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {
+  n = Number(n)
+  for (let num = 1; num <= n; num++) {
+    if (num % 5 === 0 && num % 3 === 0) {
+      console.log('fizzbuzz')
+    } else {
+      if (num % 3 === 0) {
+        console.log('fizz')
+      } else if (num % 5 === 0) {
+        console.log('buzz')
+      } else {
+        console.log(num)
+      }
+    }
+  }
+}
 
 module.exports = fizzBuzz;
--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,19 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let char = {}
+  for (let characters of str) {
+    char[characters] = char[characters] + 1 || 1
+  }
+  let maxCharCount = 0, maxChar
+  for (const [character, repetitionFig] of Object.entries(char)) {
+    if (maxCharCount < repetitionFig) {
+      maxCharCount = repetitionFig
+      maxChar = character
+    }
+  }
+  return maxChar
+}
 
 module.exports = maxChar;
