bca703c8fd867005cb31cd4c45603c42361330ff
Ashish Pradhan <ashishpradhan@Ashishs-MacBook-Air.local>
2023-11-25 12:40:33 +0530

    Added twoSum folder to it and changes in fizzBuzz

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -13,18 +13,17 @@
 //   buzz
 
 function fizzBuzz(n) {
-  n = Number(n)
   for (let num = 1; num <= n; num++) {
     if (num % 5 === 0 && num % 3 === 0) {
       console.log('fizzbuzz')
+    }
+    else if (num % 3 === 0) {
+      console.log('fizz')
+    }
+    else if (num % 5 === 0) {
+      console.log('buzz')
     } else {
-      if (num % 3 === 0) {
-        console.log('fizz')
-      } else if (num % 5 === 0) {
-        console.log('buzz')
-      } else {
-        console.log(num)
-      }
+      console.log(num)
     }
   }
 }
