a170c0229fcd8e80892175f51ec902b78d79e5e9
ashish-ad <ashishad14@gmail.com>
2024-07-15 09:08:44 +0530

    Improved according to JSlinter.

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,13 +8,17 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
+/**
+ *This method is to rever a Interger number.
+ * @param {int} n
+ * @return {n}
+ */
 function reverseInt(n) {
-  let reverseNumber = n.toString().split('').reverse().join('')
-  return parseInt(reverseNumber) * Math.sign(n)
+  const reverseNumber = n.toString().split('').reverse().join('');
+  return parseInt(reverseNumber) * Math.sign(n);
 }
 
 module.exports = reverseInt;
-
 
 
 /** One of the solution I thought of which is one way of solving the question.
